3a9ef14df79d3bea579f44c13bd16f2c3fb238d1
Mahmoud Ahmed Abdo <103747858+MahmodAhmed1@users.noreply.github.com>
2023-04-25 21:23:24 +0200

    Add files via upload

---
+++ Student Affairs/src/test/car.java
@@ -0,0 +1,15 @@
+package test;
+
+public class car {
+	int speeded;	
+	public car(){
+		
+	}
+	public void setspeed(int x) {
+		speeded=x;
+	}
+	public void getspeed()
+	{
+		System.out.println(speeded);
+	}
+}
---
+++ Student Affairs/week1/test/Class_One.java
@@ -0,0 +1,15 @@
+package test;
+import java.util.Scanner;
+
+public class Class_One {
+
+	public static void main(String args[])
+	{
+		car obj = new car();
+		obj.setspeed(30);
+		obj.getspeed();
+		
+	}
+	
+
+}
